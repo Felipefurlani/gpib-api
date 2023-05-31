4af54cca86b01238a5e07118c5ed5cac758fea43
Chaba2005 <rtromero.sp@gmail.com>
2023-05-31 09:02:54 -0300

    fon

--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,12 @@
 import mysql from 'mysql2/promise'
+import { DB } from './enviroment';
 
 let con: mysql.Connection;
 
 try {
-    con = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        port: parseInt(process.env.DB_PORT || "3306")
-    });
+    con = await mysql.createConnection(DB);
 } catch (error) {
-    console.error("Verifique as credenciais do banco de dados")
+    console.error("Verifique as credenciais do banco de dados", error)
     process.exit(1);
 }
 
