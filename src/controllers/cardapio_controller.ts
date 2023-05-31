99ecc3d646468fe4165ad06904b7037444787cc6
Chaba <rtromer.sp@gmail.com>
2023-05-31 20:00:14 -0300

    fon

--- src/controllers/cardapio_controller.ts
+++ src/controllers/cardapio_controller.ts
@@ -2,5 +2,8 @@ import database from "../database";
 
 export async function getCardapio(data: String){
     const [cardapio] = await database.execute('SELECT principal,guarnicao,salada,sobremesa,suco,periodo,vegetariano FROM Cardapio WHERE data = ?',[data])
+    if(!cardapio[0]) {
+        throw new Error('Cardápio indisponível!')
+    }
     return cardapio;
 }
