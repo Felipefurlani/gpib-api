dd736abfdcdb7cf1f3b507c52540909ed52889de
Mat2302 <mvfshoto@gmail.com>
2023-06-06 08:54:12 -0300

    correcao de erro

--- src/routes/date.ts
+++ src/routes/date.ts
@@ -10,6 +10,10 @@ router.get('/feriado/:data', async (req, res, next) => {
     if (isNaN(data.getTime())) {
         res.status(400).send("Formato de data inválido.");
     }
+
+    if (data.getFullYear() >= 1900 && data.getFullYear() <= 2100) {
+        res.status(400).send("Ano inválido.");
+    }
     
     res.status(200).json(await isHoliday(data).catch(next));
 })
