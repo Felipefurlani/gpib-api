e1745b85f40be9c1e461c2e1291b7a9f38ccfe10
Meida <99834864+AmeMeida@users.noreply.github.com>
2023-07-07 01:47:39 -0300

    formatação

--- src/routes/date.ts
+++ src/routes/date.ts
@@ -1,21 +1,20 @@
 import { Router } from "express";
 import { isHoliday } from "../controllers/date_controller";
 
-
 const router = Router();
 
-router.get('/feriado/:data', async (req, res, next) => {
-    const data = new Date(req.params.data);
+router.get("/feriado/:data", async (req, res, next) => {
+  const data = new Date(req.params.data);
 
-    if (isNaN(data.getTime())) {
-        res.status(400).send("Formato de data inválido.");
-    }
+  if (isNaN(data.getTime())) {
+    res.status(400).send("Formato de data inválido.");
+  }
 
-    if (data.getFullYear() <= 1900 || data.getFullYear() >= 2100) {
-        res.status(400).send("Ano inválido.");
-    }
-    
-    res.status(200).json(await isHoliday(data).catch(next));
-})
+  if (data.getFullYear() <= 1900 || data.getFullYear() >= 2100) {
+    res.status(400).send("Ano inválido.");
+  }
 
-export default router
+  res.status(200).json(await isHoliday(data).catch(next));
+});
+
+export default router;
