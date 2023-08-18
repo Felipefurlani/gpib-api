21b815236b1132711f98c52c6ea9331045360e6a
nallin1 <nallinguilherme@gmail.com>
2023-08-18 09:13:26 -0300

    rotas_user_peri

--- src/models/user_peri.ts
+++ src/models/user_peri.ts
@@ -3,54 +3,14 @@ import { ColumnType, Generated, Insertable, Selectable, Updateable } from 'kysel
 export interface periUserTable {
     id: Generated<number>;
     name: ColumnType<string>;
-
-    birth: ColumnType<Date>;
+    birth: ColumnType<string>;
     created_at: ColumnType<Date, string | undefined, never>;
     email: ColumnType<string>;
     password: ColumnType<string>;
     n_followers: ColumnType<number>;
     active: ColumnType<boolean>;
-
 }
 
 export type periUser = Selectable<periUserTable>;
 export type newPeriUser = Insertable<periUserTable>;
 export type updatePeriUser = Updateable<periUserTable>;
-
-/*
-export class UserPeri {
-    id: number;
-    name: string;
-    birth: Date;
-    email: string;
-    password: string;
-    created_at: Date;
-    n_followers: number;
-    active: boolean;
-
-    constructor(id: number, name: string, birth: Date, email: string, password: string, created_at: Date, n_followers: number, active: boolean) {
-        this.id = id;
-        this.name = name;
-        this.birth = birth;
-        this.email = email;
-        this.password = password;
-        this.created_at = created_at;
-        this.n_followers = n_followers;
-        this.active = active;
-    }
-
-    getAge(): number {
-        const now = new Date();
-        const age = now.getFullYear() - this.birth.getFullYear();
-
-        if (
-            now.getMonth() < this.birth.getMonth() &&
-            now.getDay() < this.birth.getDay()
-        )
-            return age - 1;
-
-        return age;
-    }
-
-}
-*/
--- src/routes/user_peri_route.ts
+++ src/routes/user_peri_route.ts
@@ -12,4 +12,40 @@ router.get("/", async (req, res) => {
     res.send(await user_peri_controller.getAll());
 });
 
+router.post("/login", async (req, res) => {
+    const credentials = req.body;
+    
+    if (!credentials.email || !credentials.password) {
+        res.status(400).send("email and password are required");
+        return;
+    }
+    if (!(await user_peri_controller.login(credentials.email, credentials.password))) {
+        res.status(401).send("invalid credentials");
+        return;
+    } else {
+        res.send("login successful");
+        res.cookie("email", credentials.email);
+    }
+});
+
+router.post("/", async (req, res) => {
+    const userPeri = req.body;
+    res.send(await user_peri_controller.create(userPeri));
+});
+
+router.patch("/", async (req, res) => {
+    const userPeri = req.body;
+    res.send(await user_peri_controller.deleteById(Number(userPeri.id)));
+});
+
+// not working
+router.get("/whoami", async (req, res) => {
+    const email = req.cookies.email;
+    if (!email) {
+        res.status(401).send("not logged in");
+        return;
+    }
+    res.send("logged with email: " + email);
+});
+
 export default router;
